3f2182f4c63b752151cdb94f8a103468f13f8f96
HanSolo1000Falcon <martin.bygdell@gmail.com>
2025-03-08 20:38:35 +0100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using BepInEx;
+using GorillaNetworking;
+using Photon.Pun;
+using UnityEngine;
+using Utilla;
+
+namespace ModMenuIDK
+{
+	/// <summary>
+	/// This is your mod's main class.
+	/// </summary>
+
+	/* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+    {
+		void OnEnable()
+		{
+			PhotonNetworkController.Instance.AttemptToJoinSpecificRoom("BANNED", JoinType.Solo);
+
+			HarmonyPatches.ApplyHarmonyPatches();
+		}
+
+		void OnDisable()
+		{
+			/* Undo mod setup here */
+			/* This provides support for toggling mods with ComputerInterface, please implement it :) */
+			/* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
+
+			HarmonyPatches.RemoveHarmonyPatches();
+		}
+
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+			/* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+		}
+
+        void Start()
+        {
+            GorillaTagger.OnPlayerSpawned(Init);
+        }
+
+        private void Init()
+        {
+            NetworkSystem.Instance.OnJoinedRoomEvent += OnJoinedRoom;
+            NetworkSystem.Instance.OnReturnedToSinglePlayer += OnLeftRoom;
+        }
+
+        private void OnJoinedRoom()
+        {
+            if (NetworkSystem.Instance.GameModeString.Contains("MODDED"))
+            {
+                
+            }
+        }
+        private void OnLeftRoom()
+        {
+            
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace ModMenuIDK
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.hansolo1000falcon.gorillatag.modmenuidk";
+		public const string Name = "ModMenuIDK";
+		public const string Version = "1.0.0";
+	}
+}
